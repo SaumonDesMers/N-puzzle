730123cf516f896adb6cbc2bc59cfb08f8c10d28
Simon Gaubert <sim.gaubert.sg@gmail.com>
2022-06-16 19:05:41 +0200

    some clean

--- src/astar.cpp
+++ src/astar.cpp
@@ -16,6 +16,10 @@ Node *AStar(Config cfg) {
 
     int i = 0;
 	for (; i < cfg.maxIter; i++) {
+		// printTree(root);
+		// string str;
+		// getline(cin, str);
+		// system("clear");
 		current = open.begin()->second;
 		open.erase(open.begin());
 		close[current->game] = current;
@@ -23,38 +27,19 @@ Node *AStar(Config cfg) {
 		if (current->HCost == 0)
 			break;
 
-		// vector<Game> nextTurns = current->game.getNextTurns();
+		vector<Game> nextTurns = current->game.getNextTurns();
 
-		// for (size_t i = 0; i < nextTurns.size(); i++) {
-		// 	unordered_map<string, Node *>::iterator it = close.find(nextTurns[i]);
-
-		// 	if (it == close.end()) {
-		// 		Node *child = new Node(nextTurns[i], current, current->depth + 1);
-		// 		current->childs.push_back(child);
-
-		// 		child->HCost = cfg.h(child->game, cfg.goal) * cfg.weight;
-		// 		// cout << "child Hcost = " << child->HCost << endl;
-		// 		open.insert(make_pair(child->HCost, child));
-		// 	}
-		// 	else if (it->second->depth > current->depth + 1) {
-		// 		it->second->setParent(current);
-		// 	}
-		// }
-
-
-		current->expand();
-
-		for (size_t i = 0; i < current->childs.size(); i++) {
-			Node *child = current->childs[i];
-
-			unordered_map<string, Node *>::iterator it = close.find(child->game);
+		for (size_t i = 0; i < nextTurns.size(); i++) {
+			unordered_map<string, Node *>::iterator it = close.find(nextTurns[i]);
 
 			if (it == close.end()) {
+				Node *child = new Node(nextTurns[i], current, current->depth + 1);
+				current->childs.push_back(child);
+
 				child->HCost = cfg.h(child->game, cfg.goal) * cfg.weight;
-				// cout << "child Hcost = " << child->HCost << endl;
 				open.insert(make_pair(child->HCost, child));
 			}
-			else if (it->second->depth > child->depth) {
+			else if (it->second->depth > current->depth + 1) {
 				it->second->setParent(current);
 			}
 		}
@@ -69,10 +54,10 @@ Node *AStar(Config cfg) {
 
 	end = chrono::system_clock::now();
 
-    // cout << "Opened nodes = " << open.size() << endl;
-    // cout << "Closed nodes = " << close.size() << endl;
-	printTree(root);
-    cout << "\nTotal nodes = " << close.size() + open.size() << endl;
+	// printTree(root);
+    cout << "Opened nodes = " << open.size() << endl;
+    cout << "Closed nodes = " << close.size() << endl;
+    cout << "Total nodes = " << close.size() + open.size() << endl;
     cout << "Iteration count = " << i << endl;
 	getSolution(current);
 	printTime(end-start, "execution time = ");
