f4d10a3143f33df79c1e990304acfcc759db60c4
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-09-13 19:09:31 +0200

    remove find from heuristique

--- src/heuristique.cpp
+++ src/heuristique.cpp
@@ -10,50 +10,54 @@ int badPlacedTiles(Game &game, Game &goal) {
 int manhattanDistance(Game &game, Game &goal) {
 	int val = 0;
 	for (size_t i = 1; i < game.size * game.size; i++) {
-		vec2 pos = game.find(i);
-		vec2 goalPos = goal.find(i);
-		val += abs(pos.row - goalPos.row) + abs(pos.col - goalPos.col);
+		int size = game.size;
+		val += ::abs(game.tilesPos[i] / size - goal.tilesPos[i] / size) + ::abs(game.tilesPos[i] % size - goal.tilesPos[i] % size);
 	}
 	return val;
 }
 
 int linearConflict(Game &game, Game &goal) {
 	int count = 0;
-	vector<vector<vec2>> goalPos;
+	int size = game.size;
 	// for each tiles
 	for (size_t i = 1; i < game.size * game.size; i++) {
-		vec2 pos = game.find(i);
-		vec2 goalPos = goal.find(i);
+
+		// vec2 pos = game.find(i);
+		// vec2 goalPos = goal.find(i);
+		int pos = game.tilesPos[i];
+		int goalPos = goal.tilesPos[i];
+
 		// if tile is in the correct row but wrong col
-		if (pos.row == goalPos.row/* && pos.col != goalPos.col*/) {
+		if (pos / size == goalPos / size/* && pos % size != goalPos % size*/) {
 			// for each neighbour tiles at his right
-			for (size_t col = pos.col + 1; col < game.size; col++) {
-				int n = game.grid[pos.row * game.size + col];
+			for (size_t col = pos % size + 1; col < game.size; col++) {
+				int n = game.grid[pos / size * game.size + col];
 				// pass if neighbour is the empty tile
 				if (n == 0)
 					continue;
-				vec2 nGoalPos = goal.find(n);
+				// vec2 nGoalPos = goal.find(n);
+				int nGoalPos = goal.tilesPos[n];
 				// if neighbour is in the correct row and its goal col is at the left of the current tile's goal col
-				if (pos.row == nGoalPos.row && nGoalPos.col < goalPos.col)
+				if (pos / size == nGoalPos / size && nGoalPos % size < goalPos % size)
 					count++;
 			}
 		}
 		// if tile is in the correct col but wrong row
-		if (pos.col == goalPos.col/* && pos.row != goalPos.row*/) {
+		if (pos % size == goalPos % size/* && pos / size != goalPos / size*/) {
 			// for each neighbour tiles below it
-			for (size_t row = pos.row + 1; row < game.size; row++) {
-				int n = game.grid[row * game.size + pos.col];
+			for (size_t row = pos / size + 1; row < game.size; row++) {
+				int n = game.grid[row * game.size + pos % size];
 				// pass if neighbour is the empty tile
 				if (n == 0)
 					continue;
-				vec2 nGoalPos = goal.find(n);
+				// vec2 nGoalPos = goal.find(n);
+				int nGoalPos = goal.tilesPos[n];
 				// if neighbour is in the correct col and its goal row is above the current tile's goal row
-				if (pos.col == nGoalPos.col && nGoalPos.row < goalPos.row)
+				if (pos % size == nGoalPos % size && nGoalPos / size < goalPos / size)
 					count++;
 			}
 		}
 	}
-	// cout << "Conflict count = " << count << endl;
 	return count;
 }
 
